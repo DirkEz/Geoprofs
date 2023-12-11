977d7ff039dbe271da6ee4a860086e3446573e75
DirkEz <dirk.schaafstra@gmail.com>
2023-12-11 10:36:52 +0100

    UC09 Create positie.cs
    Aanmaken van het positie Model

---
+++ geoprofs/geoprofs/Models/positie.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Positie
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Positie naam is verplicht.")]
+    [StringLength(60, ErrorMessage = "Positie naam mag niet langer zijn dan 60 karakters.")]
+    public string PositieNaam { get; set; }
+}
+
