8adb99bff69b3eff1bdde22a887bb192ff5b6880
DirkEz <dirk.schaafstra@gmail.com>
2023-12-15 09:20:49 +0100

    Create Index.cshtml.cs

---
+++ geoprofs/geoprofs/Pages/VerlofAanvragen/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using geoprofs.Data;
+using geoprofs.Models;
+
+namespace geoprofs.Pages.VerlofAanvragen
+{
+    public class IndexModel : PageModel
+    {
+        private readonly geoprofs.Data.geoprofsContext _context;
+
+        public IndexModel(geoprofs.Data.geoprofsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Aanvraag> Aanvraag { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Aanvraag != null)
+            {
+                Aanvraag = await _context.Aanvraag
+                .Include(a => a.OndersteundDoorWerknemer)
+                .Include(a => a.Werknemer).ToListAsync();
+            }
+        }
+    }
+}
