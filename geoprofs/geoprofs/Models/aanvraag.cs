7be426c08ff3542d5d6e131f319b875556b4b18e
DirkEz <dirk.schaafstra@gmail.com>
2023-12-15 09:19:52 +0100

    UC09 Models Toegevoegd

--- geoprofs/geoprofs/Models/aanvraag.cs
+++ geoprofs/geoprofs/Models/aanvraag.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Aanvraag
+namespace geoprofs.Models
 {
-    public int Id { get; set; }
+    public class Aanvraag
+    {
+        public int Id { get; set; }
 
-    [Display(Name = "Werknemer ID")]
-    public int WerknemersId { get; set; }
+        [Display(Name = "Werknemer ID")]
+        public int WerknemersId { get; set; }
 
-    [Display(Name = "Vanaf datum")]
-    [DataType(DataType.Date)]
-    public DateTime VanafDatum { get; set; }
+        [Display(Name = "Vanaf datum")]
+        [DataType(DataType.Date)]
+        public DateTime VanafDatum { get; set; }
 
-    [Display(Name = "Tot datum")]
-    [DataType(DataType.Date)]
-    public DateTime TotDatum { get; set; }
+        [Display(Name = "Tot datum")]
+        [DataType(DataType.Date)]
+        public DateTime TotDatum { get; set; }
 
-    public int Vakantie { get; set; }
+        public int Vakantie { get; set; }
 
-    public int Persoonlijk { get; set; }
+        public int Persoonlijk { get; set; }
 
-    public int Ziek { get; set; }
+        public int Ziek { get; set; }
 
-    public int Goedkeuring { get; set; }
+        public int Goedkeuring { get; set; }
 
-    [Display(Name = "Ondersteund door")]
-    public int OndersteundDoor { get; set; }
+        [Display(Name = "Ondersteund door")]
+        public int OndersteundDoor { get; set; }
 
-    public Werknemer Werknemer { get; set; }
-    public Werknemer OndersteundDoorWerknemer { get; set; }
+        [Display(Name = "Werknemer ID")]
+        public int WerknemerId { get; set; }
+
+        [Display(Name = "Ondersteund door Werknemer ID")]
+        public int OndersteundDoorWerknemerId { get; set; }
+
+        public Werknemer Werknemer { get; set; }
+
+        [ForeignKey("OndersteundDoorWerknemerId")]
+        public Werknemer OndersteundDoorWerknemer { get; set; }
+    }
 }
--- geoprofs/geoprofs/Models/positie.cs
+++ geoprofs/geoprofs/Models/positie.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class Positie
-{
-    public int Id { get; set; }
 
-    [Required(ErrorMessage = "Positie naam is verplicht.")]
-    [StringLength(60, ErrorMessage = "Positie naam mag niet langer zijn dan 60 karakters.")]
-    public string PositieNaam { get; set; }
+namespace geoprofs.Models
+{ 
+    public class Positie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Positie naam is verplicht.")]
+        [StringLength(60, ErrorMessage = "Positie naam mag niet langer zijn dan 60 karakters.")]
+        public string PositieNaam { get; set; }
+    }
 }
-
