991a3eb84b92f45a6f4c1ee2f3557e197a9ce002
DirkEz <dirk.schaafstra@gmail.com>
2023-12-15 09:19:28 +0100

    Update Program.cs

--- geoprofs/geoprofs/Program.cs
+++ geoprofs/geoprofs/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using geoprofs.Data;
 namespace geoprofs
 {
     public class Program
@@ -5,6 +8,8 @@ namespace geoprofs
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<geoprofsContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("geoprofsContext") ?? throw new InvalidOperationException("Connection string 'geoprofsContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddRazorPages();
