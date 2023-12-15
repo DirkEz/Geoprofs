c28d1ec44f3cd920697bc84d73879532e8bcd420
DirkEz <dirk.schaafstra@gmail.com>
2023-12-15 09:20:12 +0100

    Project Bestanden Geupdate

---
+++ geoprofs/geoprofs/Data/geoprofsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using geoprofs.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace geoprofs.Data
+{
+    public class geoprofsContext : DbContext
+    {
+        public geoprofsContext (DbContextOptions<geoprofsContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Aanvraag>? Aanvraag { get; set; }
+    }
+}
