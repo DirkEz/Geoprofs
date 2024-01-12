41e2cb4eac56da11ed4a6faa46f4a4e6c2676b13
bjoreno <bjodegens@hotmail.com>
2024-01-12 09:40:01 +0100

    inlog gemaakt en sessions gefixt

--- geoprofs/geoprofs/Pages/Privacy.cshtml.cs
+++ geoprofs/geoprofs/Pages/Privacy.cshtml.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace geoprofs.Pages
 {
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
+
+        public string Username => _httpContextAccessor.HttpContext.Session.GetString("Username");
 
         public void OnGet()
         {
+            _logger.LogInformation($"Username retrieved from session: {Username}");
         }
     }
 }
+
+
