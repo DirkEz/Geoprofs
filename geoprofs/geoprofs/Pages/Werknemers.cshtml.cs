7337a4021c578f0dd51a1c375668e51673f34d87
DirkEz <dirk.schaafstra@gmail.com>
2024-01-15 10:54:49 +0100

    Create Werknemers.cshtml.cs

---
+++ geoprofs/geoprofs/Pages/Werknemers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace geoprofs.Pages
+{
+    public class Werknemers : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
