fa1eb0494d28f06d7ebef44d3af46ffd99b8d3d2
DirkEz <dirk.schaafstra@gmail.com>
2024-01-19 10:51:00 +0100

    Update Werknemers.cshtml.cs

--- geoprofs/geoprofs/Pages/Werknemers.cshtml.cs
+++ geoprofs/geoprofs/Pages/Werknemers.cshtml.cs
@@ -1,12 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using geoprofs.Data;
+using geoprofs.Models;
 
-namespace geoprofs.Pages
+namespace geoprofs.Werknemers
 {
-    public class Werknemers : PageModel
+    public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly geoprofs.Data.geoprofsContext _context;
+
+        public IndexModel(geoprofs.Data.geoprofsContext context)
         {
+            _context = context;
+        }
+
+        public IList<Aanvraag> Aanvraag { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Aanvraag != null)
+            {
+                Aanvraag = await _context.Aanvraag
+                .Include(a => a.OndersteundDoorWerknemer)
+                .Include(a => a.Werknemer).ToListAsync();
+            }
         }
     }
 }
